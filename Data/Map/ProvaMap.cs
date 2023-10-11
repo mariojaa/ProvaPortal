e438246f8d6b025325204a7f613410eacdab0591
marioaraujougb <mario.araujo@ugb.edu.br>
2023-10-11 20:32:50 -0300

    Criado ProvaMap

---
+++ Data/Map/ProvaMap.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProvaPortal.Data.Map
+{
+    public class ProvaMap : IEntityTypeConfiguration<ProvaModel>
+    {
+        public void Configure(EntityTypeBuilder<ProvaModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            //builder.Property(x => x.NomeCompleto).HasMaxLength(255).IsRequired();
+            builder.HasOne(x => x.NomeArquivo);
+        }
+    }
+}
